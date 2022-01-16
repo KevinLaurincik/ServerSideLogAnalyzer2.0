2f4b41624e11a859eeaee76d5c010e122179c47b
Kevin Laurinčík <kevin.laurincik@gmail.com>
2022-01-16 10:19:48 +0100

    first commmit

---
+++ ServerSideLogAnalyzer/Models/LogModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerSideLogAnalyzer.Models
+{
+    public class LogModel
+    {
+        public int idLogs { get; set; }
+        public DateTime Date { get; set; }
+        public string Discription { get; set; }
+    }
+}
---
+++ ServerSideLogAnalyzer/wwwroot/js/MyJs.js
@@ -0,0 +1,6 @@
+﻿function createAlert() {
+    alert("ID musí byť číselná hodnota od 1 do 5 000 000. Dátum zadajte v tvare MM-DD-RRRR(Mesiac-Deň-Rok). Popis môže obsahovať maximálne 2000 znakov!")
+}
+function focusOnElement(element) {
+    element.focus();
+}
